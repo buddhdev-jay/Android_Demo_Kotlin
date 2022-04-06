bfe128fac5ff86b9598b1a60a6085fe2ec9e6b2b
buddhdev-jay <jay.b@simformsolutions.com>
2022-04-06 19:22:23 +0530

    TE5-T679 Form Implemented

--- app/src/main/java/com/example/android_demo_kotlin/databinding/BindingViewModel.kt
+++ app/src/main/java/com/example/android_demo_kotlin/databinding/BindingViewModel.kt
@@ -1,35 +1,37 @@
 package com.example.android_demo_kotlin.databinding
 
+import android.text.Editable
 import android.text.TextUtils
+import android.text.TextWatcher
 import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
-import androidx.databinding.ObservableArrayList
+import androidx.databinding.InverseBindingListener
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
-class BindingViewModel:ViewModel() {
+class BindingViewModel : ViewModel() {
 
     var intvalue = ""
-    val name = MutableLiveData<String>("abc")
+    val name = MutableLiveData("abc")
 
     var email: String = ""
     var password: String = ""
 
     private val logInResult = MutableLiveData<String>()
-    val formHints = ObservableArrayList<Hints>()
 
     fun getLogInResult(): LiveData<String> = logInResult
 
     @BindingAdapter("android:text")
-    fun EditText.integerValue(value: Int){
+    fun EditText.integerValue(value: Int) {
         this.setText(value.toString())
     }
 
     @InverseBindingAdapter(attribute = "android:text")
-    fun getstring(input: String):String{
+    fun getstring(input: String): String {
         intvalue = input
         return intvalue
     }
@@ -53,13 +55,27 @@ class BindingViewModel:ViewModel() {
         }
         logInResult.value = "Valid credentials :)"
     }
+
     @BindingAdapter(" android:hint")
     fun setHintMessage(view: EditText, Message: String) {
         view.hint = Message
     }
-    enum class Hints {
-        EMAIL,
-        PASSWORD
+
+    @BindingAdapter("android:textAttrChanged")
+    fun setTextChangedListener(editText: EditText, listener: InverseBindingListener) {
+        editText.addTextChangedListener(object: TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                listener.onChange()
+            }
+        })
     }
+
 
 }
--- app/src/main/java/com/example/android_demo_kotlin/databinding/DataBindingActivity.kt
+++ app/src/main/java/com/example/android_demo_kotlin/databinding/DataBindingActivity.kt
@@ -4,15 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.android_demo_kotlin.R
+import kotlinx.android.synthetic.main.activity_data_binding.btn_login_data_binding
 
 
-class DataBindingActivity : AppCompatActivity() {
+class DataBindingActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var binding: ActivityDataBindingBinding
     val bindingViewModel: BindingViewModel by viewModels()
@@ -21,10 +23,19 @@ class DataBindingActivity : AppCompatActivity() {
 
        binding = DataBindingUtil.setContentView(this,R.layout.activity_data_binding)
         binding.viewModels = bindingViewModel
+        binding.clickHandler = this
         binding.lifecycleOwner = this
-        bindingViewModel.getLogInResult().observe(this, Observer { result ->
+        bindingViewModel.getLogInResult().observe(this) { result ->
             Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
-        })
+        }
 
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            R.id.btn_login_data_binding -> {
+                bindingViewModel.performValidation()
+            }
+        }
     }
 }
