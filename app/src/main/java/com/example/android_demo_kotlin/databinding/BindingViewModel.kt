fc7e283e44b9c55ada53e9d197b23eeb0997115e
buddhdev-jay <jay.b@simformsolutions.com>
2022-04-06 15:42:59 +0530

    TE5-T679 Validation Form Done

--- app/src/main/java/com/example/android_demo_kotlin/databinding/BindingViewModel.kt
+++ app/src/main/java/com/example/android_demo_kotlin/databinding/BindingViewModel.kt
@@ -1,25 +1,65 @@
 package com.example.android_demo_kotlin.databinding
 
+import android.text.TextUtils
 import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.bumptech.glide.Glide
+
 
 class BindingViewModel:ViewModel() {
 
     var intvalue = ""
-    val name = MutableLiveData<String>()
+    val name = MutableLiveData<String>("abc")
+
+    var email: String = ""
+    var password: String = ""
+
+    private val logInResult = MutableLiveData<String>()
+    val formHints = ObservableArrayList<Hints>()
+
+    fun getLogInResult(): LiveData<String> = logInResult
+
     @BindingAdapter("android:text")
     fun EditText.integerValue(value: Int){
         this.setText(value.toString())
     }
+
     @InverseBindingAdapter(attribute = "android:text")
     fun getstring(input: String):String{
         intvalue = input
         return intvalue
     }
+
+    @BindingAdapter("emailValidator")
+    fun emailValidator(editText: EditText, email: String?) {
+        if (TextUtils.isEmpty(email)) {
+            editText.error = "Please Enter Email"
+            return
+        }
+    }
+
+    fun performValidation() {
+        if (email.isBlank()) {
+            logInResult.value = "Invalid username"
+            return
+        }
+        if (password.isBlank()) {
+            logInResult.value = "Invalid password"
+            return
+        }
+        logInResult.value = "Valid credentials :)"
+    }
+    @BindingAdapter(" android:hint")
+    fun setHintMessage(view: EditText, Message: String) {
+        view.hint = Message
+    }
+    enum class Hints {
+        EMAIL,
+        PASSWORD
+    }
+
 }
--- app/src/main/java/com/example/android_demo_kotlin/databinding/DataBindingActivity.kt
+++ app/src/main/java/com/example/android_demo_kotlin/databinding/DataBindingActivity.kt
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.android_demo_kotlin.R
 
 
@@ -20,7 +22,9 @@ class DataBindingActivity : AppCompatActivity() {
        binding = DataBindingUtil.setContentView(this,R.layout.activity_data_binding)
         binding.viewModels = bindingViewModel
         binding.lifecycleOwner = this
-
+        bindingViewModel.getLogInResult().observe(this, Observer { result ->
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+        })
 
     }
 }
