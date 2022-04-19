f4897357a76935d3e6aba4f705b6ea084d00ed96
buddhdev-jay <jay.b@simformsolutions.com>
2022-04-19 19:05:07 +0530

    Coordinator Done

--- app/src/main/java/com/example/android_demo_kotlin/NavigationViewActivity.kt
+++ app/src/main/java/com/example/android_demo_kotlin/NavigationViewActivity.kt
@@ -6,37 +6,37 @@ import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
-import kotlinx.android.synthetic.main.activity_navigation_view.drawerlayout
+import kotlinx.android.synthetic.main.activity_navigation_view.drawer_layout
 import kotlinx.android.synthetic.main.activity_navigation_view.nav_view
 
 class NavigationViewActivity : AppCompatActivity() {
 
-    private lateinit var actionBarToggle: ActionBarDrawerToggle
+    private lateinit var actionBarToggle1: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation_view)
 
-        actionBarToggle = ActionBarDrawerToggle(this, drawerlayout, 0, 0)
-        drawerlayout.addDrawerListener(actionBarToggle)
+        actionBarToggle1 = ActionBarDrawerToggle(this, drawer_layout, 0, 0)
+        drawer_layout.addDrawerListener(actionBarToggle1)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        actionBarToggle.syncState()
+        actionBarToggle1.syncState()
 
         nav_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.myProfile -> {
                     Toast.makeText(this, "My Profile", Toast.LENGTH_SHORT).show()
-                    drawerlayout.closeDrawer(GravityCompat.START)
+                    drawer_layout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.people -> {
                     Toast.makeText(this, "People", Toast.LENGTH_SHORT).show()
-                    drawerlayout.closeDrawer(GravityCompat.START)
+                    drawer_layout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.settings -> {
                     Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
-                    drawerlayout.closeDrawer(GravityCompat.START)
+                    drawer_layout.closeDrawer(GravityCompat.START)
                     true
                 }
                 else -> {
@@ -48,14 +48,14 @@ class NavigationViewActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        drawerlayout.openDrawer(nav_view)
+        drawer_layout.openDrawer(nav_view)
         return true
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
-        if(drawerlayout.isDrawerOpen(GravityCompat.START)){
-            drawerlayout.closeDrawer(GravityCompat.START)
+        if(drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawer(GravityCompat.START)
         } else {
             super.onBackPressed()
         }
